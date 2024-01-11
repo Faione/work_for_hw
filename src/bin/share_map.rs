15d47a728a5d1ec3b0ee9dc9f93d77730c94adf2
Fhl <1287481902@qq.com>
2024-01-11 21:03:17 +0800

    修复 apply 无法正确识别cgroup的问题

--- src/bin/share_map.rs
+++ src/bin/share_map.rs
@@ -3,7 +3,12 @@ mod share_map {
     include!(concat!(env!("OUT_DIR"), "/share_map.skel.rs"));
 }
 
-use std::{collections::BTreeMap, env, fs::read_to_string, os::unix::prelude::MetadataExt};
+use std::{
+    collections::BTreeMap,
+    env,
+    fs::{read_dir, read_to_string},
+    os::unix::prelude::MetadataExt,
+};
 
 use anyhow::Ok;
 use clap::{Args, Parser, Subcommand};
@@ -75,9 +80,54 @@ fn init_libbpf_log() {
     )));
 }
 
-fn read_cgroup_inode_id(path: String) -> anyhow::Result<u64> {
+fn read_cgroup_inode_id(path: &str) -> anyhow::Result<u64> {
+    println!("read cgroup: {}", path);
     let id = std::fs::metadata(path)?.ino();
     Ok(id)
+}
+
+#[derive(Debug)]
+struct VMCgroup {
+    emulator: String,
+    vcpus: Vec<String>,
+}
+
+fn find_vm_cgroup(path: &str) -> anyhow::Result<VMCgroup> {
+    let libvirt_path = path.to_owned() + "/libvirt";
+
+    let mut vm_cgroup = VMCgroup {
+        emulator: String::new(),
+        vcpus: Vec::<String>::new(),
+    };
+
+    read_dir(libvirt_path)?.for_each(|dir| {
+        if let Result::Ok(cgroup) = dir {
+            let cgroup_path = cgroup.path().into_os_string().into_string().unwrap();
+            let cgroup_name = cgroup.file_name().into_string().unwrap();
+
+            if cgroup_name == "emulator" {
+                vm_cgroup.emulator = cgroup_path;
+            } else if cgroup_name.starts_with("vcpu") {
+                vm_cgroup.vcpus.push(cgroup_path);
+            }
+        }
+    });
+
+    Ok(vm_cgroup)
+}
+
+#[cfg(test)]
+mod test {
+    use crate::find_vm_cgroup;
+
+    #[test]
+    fn read_vm_cgroup() {
+        let dummy_path = "/tmp/test_vm";
+
+        let vm_cgroup = find_vm_cgroup(dummy_path);
+
+        println!("{:#?}", vm_cgroup);
+    }
 }
 
 fn main() -> anyhow::Result<()> {
@@ -96,7 +146,7 @@ fn main() -> anyhow::Result<()> {
 
     match opts.action {
         Action::Insert(args) => args.cgroup.into_iter().for_each(|path| {
-            if let Result::Ok(cg_id) = read_cgroup_inode_id(path) {
+            if let Result::Ok(cg_id) = read_cgroup_inode_id(&path) {
                 let cgroup_id = cg_id.to_le_bytes();
                 cgroup_map
                     .update(&cgroup_id, &cgroup_id, MapFlags::NO_EXIST)
@@ -104,7 +154,7 @@ fn main() -> anyhow::Result<()> {
             };
         }),
         Action::Delete(args) => args.cgroup.into_iter().for_each(|path| {
-            if let Result::Ok(cg_id) = read_cgroup_inode_id(path) {
+            if let Result::Ok(cg_id) = read_cgroup_inode_id(&path) {
                 let cgroup_id = cg_id.to_le_bytes();
                 cgroup_map
                     .delete(&cgroup_id)
@@ -112,7 +162,7 @@ fn main() -> anyhow::Result<()> {
             };
         }),
         Action::Get(args) => args.cgroup.into_iter().for_each(|path| {
-            if let Result::Ok(cg_id) = read_cgroup_inode_id(path) {
+            if let Result::Ok(cg_id) = read_cgroup_inode_id(&path) {
                 let cgroup_id = cg_id.to_le_bytes();
                 let _ = cgroup_map
                     .lookup(&cgroup_id, MapFlags::ANY)
@@ -149,12 +199,23 @@ fn main() -> anyhow::Result<()> {
             let config: Config = serde_yaml::from_str(&yaml).unwrap();
 
             config.vm_infos.iter().for_each(|(_, vm_info)| {
-                if let Result::Ok(cg_id) = read_cgroup_inode_id(vm_info.cgroup.clone()) {
+                let vm_cgroup = find_vm_cgroup(&vm_info.cgroup).expect("not a valid vm cgroup");
+
+                if let Result::Ok(cg_id) = read_cgroup_inode_id(&vm_cgroup.emulator) {
                     let cgroup_id = cg_id.to_le_bytes();
                     cgroup_map
                         .update(&cgroup_id, &cgroup_id, MapFlags::NO_EXIST)
                         .expect("insert element to map failed");
                 };
+
+                vm_cgroup.vcpus.iter().for_each(|cgroup| {
+                    if let Result::Ok(cg_id) = read_cgroup_inode_id(cgroup) {
+                        let cgroup_id = cg_id.to_le_bytes();
+                        cgroup_map
+                            .update(&cgroup_id, &cgroup_id, MapFlags::NO_EXIST)
+                            .expect("insert element to map failed");
+                    };
+                })
             })
         }
     }
